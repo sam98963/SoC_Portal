af4c215da6cd9b146161f472eb30eb4ecb44c900
ahiajalil <ahiajalil@gmail.com>
2023-04-25 14:04:16 +0100

    updated data file to add task object

--- soc-portal/src/components/data/data.js
+++ soc-portal/src/components/data/data.js
@@ -1,4 +1,4 @@
-export const userdata =  [  
+export const loginData =  [  
   {
       firstName: "Hani",
       surname: "Abdi",
@@ -28,3 +28,47 @@ export const userdata =  [
       password: "1234"
   },  
 ]  
+
+export const userData = [
+
+    {
+        firstName: "Hung",
+        surname: "Vo",
+        email: "hungv@gmail.com",
+        appId: "123456",
+        toDoList: [ 
+            { task1: "make avatar", dueDate: "25/04/2023", completed: false},
+            { task2:" make game", dueDate: "30/04/2023", completed: false, }
+        ]
+    },
+    {
+        firstName: "Ahia",
+        surname: "Jalil",
+        email: "ahiajalil@gmail.com",
+        appId: "147852",
+        toDoList: [ 
+            { task1: "make avatar", dueDate: "25/04/2023", completed: true },
+            { task2:" make game", dueDate: "30/04/2023", completed: false, }
+        ]
+    },
+    {
+        firstName: "Samuel",
+        surname: "Willimason",
+        email: "samw@gmail.com",
+        appId: "369852",
+        toDoList: [ 
+            { task1: "make avatar", dueDate: "25/04/2023", completed: true },
+            { task2:" make game", dueDate: "30/04/2023", completed: true, }
+        ]
+    },
+    {
+        firstName: "Hani",
+        surname: "Abdi",
+        email: "hani@gmail.com",
+        appId: "456321",
+        toDoList: [ 
+            { task1: "make avatar", dueDate: "25/04/2023", completed: true },
+            { task2:" make game", dueDate: "30/04/2023", completed: true, }
+        ]
+    }
+]
